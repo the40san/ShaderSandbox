ac76db81fc53a7dce2f390496703a175a277aa40
Masashi AKISUE <masashi.akisue@aktsk.jp>
2017-02-22 13:09:58 +0900

    let's rotate cube

---
+++ Assets/Assets/Scripts/Cube.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour {
+	public float rotateSpeedX = 10f;
+	public float rotateSpeedY = 10f;
+	public float rotateSpeedZ = 10f;
+
+	void Update () {
+		transform.Rotate(new Vector3(rotateSpeedX, rotateSpeedY, rotateSpeedZ) * Time.deltaTime);
+	}
+}
